71c4aa1a398555efda3a61481e4e3016992a5d3b
Raj <bitturaj1990@gmail.com>
2025-03-30 15:55:25 +0530

    initial commit

---
+++ src/main/java/com/rllgf/boot/demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.rllgf.boot.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/message")
+	public String testMsg() {
+		return "testing ok-------------------";
+	}
+}
